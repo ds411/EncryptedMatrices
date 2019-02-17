64f41852f2fb4103e07f1fbdc6cae6706029f8c3
Dan Sullivan <srnoseman@gmail.com>
2019-02-17 17:52:20 -0500

    Added simpleRebuild method to rebuild full table on update

--- src/main/java/com/sse/dynamicssedropbox/resources/Tree.java
+++ src/main/java/com/sse/dynamicssedropbox/resources/Tree.java
@@ -34,4 +34,17 @@ public class Tree {
         return idSet;
     }
 
+    public void simpleRebuild(BigInteger[][] values) {
+        //lg(n) + 1 levels
+        TreeLevel[] newLevels = new TreeLevel[(int)Math.ceil(Math.log(values.length) / Math.log(2)) + 1];
+        for(int i = 0; i < newLevels.length; i++) {
+            newLevels = new TreeLevel[i];
+        }
+        Value[] newValues = new Value[values.length];
+        for(int i = 0; i < newValues.length; i++) {
+            newValues[i] = new Value(values[i][0], values[i][1], values[i][2]);
+        }
+        newLevels[newLevels.length-1].values = newValues;
+        levels = newLevels;
+    }
 }
