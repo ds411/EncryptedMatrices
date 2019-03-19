d90eb94d3f98ffac4c97dd0efa5f1ff3f35ac14e
ds411 <33884603+ds411@users.noreply.github.com>
2019-03-19 10:52:17 -0400

    Initial commit

---
+++ src/main/java/com/sse/structuredencryption/StructuredencryptionApplication.java
@@ -0,0 +1,13 @@
+package com.sse.structuredencryption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StructuredencryptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StructuredencryptionApplication.class, args);
+    }
+
+}
